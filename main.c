be1b4f3d5b73f42832622b51b4598923eaf115ae
borna-z <borna8688@gmail.com>
2026-04-25 03:44:07 +0200

    Antagnings prov C

--- main.c
+++ main.c
@@ -1,2 +1,62 @@
-//Write your code in this file
+#include <stdio.h>
+#include <ctype.h>
 
+#define STUDENTS 5
+#define TEST 13
+#define NAME_LEN 11
+
+void capitalize (char name[]){
+    if(name[0] != '\0'){
+        name[0] = toupper(name[0]);
+    }
+
+    for (int i = 1; name[i] != '\0'; i++){
+        name[i] = tolower(name[i]);
+    }
+
+}
+
+int main () {
+    char names[STUDENTS][NAME_LEN];
+    int scores[STUDENTS][TEST];
+    double averages[STUDENTS];
+
+    int best_index = 0;
+    double best_average = 0.0;
+    double group_average = 0.0;
+
+    for (int i = 0; i < STUDENTS; i++){
+        int sum = 0;
+
+        scanf ("%10s", names[i]);
+
+        for (int j = 0; j < TEST; j++){
+            scanf("%d", &scores[i][j]);
+            sum += scores[i][j];
+        }
+
+        averages[i] = sum / (double)TEST;
+        group_average += averages[i];
+
+        if (i  == 0 || averages[i] > best_average){
+            best_average = averages[i];
+            best_index = i;
+        }
+    }
+
+    group_average /= STUDENTS;
+
+    capitalize(names[best_index]);
+    printf("%s\n", names[best_index]);
+
+    for (int i = 0; i < STUDENTS; i++){
+        if (averages[i] < group_average){
+            capitalize(names[i]);
+            printf("%s\n", names[i]);
+        }
+    }
+    return 0;
+}
+
+//Kul med lite debugging också 
+//Hoppas vi ses snart igen! :)
